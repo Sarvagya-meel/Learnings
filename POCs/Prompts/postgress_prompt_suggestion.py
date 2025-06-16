b9afbbc3e1e061bbf2270ce6ff1bf4e7de3f4a9c
Sarvagya-meel <60498173+Sarvagya-meel@users.noreply.github.com>
2025-06-16 20:07:40 +0530

    learning/google adk

---
+++ POCs/Prompts/postgress_prompt_suggestion.py
@@ -0,0 +1,8 @@
+"""
+Author: Sarvagya Meel
+Email: sarvagyameel2@gmail.com
+Date: 05/02/25
+"""
+
+if __name__ == '__main__':
+    print('Python')
---
+++ POCs/retrievers/contextual_compressor.py
@@ -0,0 +1,8 @@
+"""
+Author: Sarvagya Meel
+Email: sarvagyameel2@gmail.com
+Date: 6/6/2025s
+"""
+
+if __name__ == '__main__':
+    print('Python')
