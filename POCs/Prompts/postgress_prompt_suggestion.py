39e4a48bf4b5bc0b8b579488d138a7eca33c5184
Sarvagya Meel <sarvagyameel2@gmail..com>
2025-06-16 20:17:30 +0530

    PROMPT SUGGESTION QUERY

--- POCs/Prompts/postgress_prompt_suggestion.py
+++ POCs/Prompts/postgress_prompt_suggestion.py
@@ -4,5 +4,40 @@ Email: sarvagyameel2@gmail.com
 Date: 05/02/25
 """
 
+
+suggesttons,
+keyworas = 1, W
+curr = self. conn[ connection'). cursor)
+repts = (l", "), (", "), C#*.
+"*),
+('ameriprise', "'), C'advisorcompass'.
+"'), C'evankilow', ")
+ngrams = reduce(Lambda a, kv: a. replace(*kv), repts, ngrams. lower())
+try:
+sqL = f"""WITH strip query_cte AS (
+SELECT strip(to_tsvector('english', '{ngrams}'))::text AS query_vector_text
+unnest query-cte as (select unnest(string_to_array(query_vector_text, ' •)) as word from strip_query_cte
+stopword_removaL_cte as (select word from unnest-query_cte where word not in (select concat('''' ,stopword, '''') from {self.stopword_table) where app_id = 'ask")
+tv_cte as ( select string_agg(word, • •) as processed_query from stopword_removal_cte
+ranked_prompts AS (
+SELECT
+ser_id, artcLid, keywords, prompt_suggestion, ts_rank(
+to tsvector('english', prompt_suggestion || • • || keywords), to_tsquery ('english', (
+SELECT replace(processed_query,'', '=&') || ':' AS user_query FROM tv_cte
+) AS RANK
+FROM {self-heading_table}
+)
+SELECT
+ser_id, artclid, keywords, prompt_suggestion, rank
+FROM ranked_prompts
+WHERE rank > 1.0-20
+ORDER BY rank DESC
+LIMIT {top_k_prompt}""";
+CurT . execute (sqL)
+results = curr. fetchall(
+if len(results) == 0:
+raise RuntimeWarning ("No results found 
+
+
 if __name__ == '__main__':
     print('Python')
